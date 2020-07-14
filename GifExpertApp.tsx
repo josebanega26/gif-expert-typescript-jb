09166e291e9e8d8efa2ce7488ff42d2e4933c97c
josebanega26 <banegajose26@gmail.com>
2020-07-14 00:51:16 -0300

    Add Categories

--- GifExpertApp.tsx
+++ GifExpertApp.tsx
@@ -1,10 +1,17 @@
-import React, { Fragment } from "react";
+import React, { Fragment, useState } from "react";
 import Categories from "./Categories";
 
 const GiftExpertApp = () => {
-  const categories: string[] = ["Star wars", "Lord of rings"];
+  const [categories, setCategories] = useState(["Star wars", "Lord of rings"]);
+
+  const handleAdd = () => {
+    const category: string = "HxH"
+    console.log('added')
+    setCategories(oldCategories => [...oldCategories, category]);
+  };
   return (
     <Fragment>
+      <button onClick={handleAdd}>add</button>
       <h2>Gif Expert App</h2>
       <hr />
       <Categories data={categories} />
