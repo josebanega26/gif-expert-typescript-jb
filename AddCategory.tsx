fe8d7248a6c9418e4d76aaebc8ce7da304b637ac
josebanega26 <banegajose26@gmail.com>
2020-07-17 23:03:29 -0300

    2 files modified

--- AddCategory.tsx
+++ AddCategory.tsx
@@ -1,24 +1,34 @@
-import React,{useState, ChangeEvent, FormEvent} from 'react'
+import React, { useState, ChangeEvent, FormEvent } from "react";
 
-const AddCategory = () => {
-    const [input, setInput] = useState('hey')
-    const changeInput = (e:ChangeEvent<HTMLInputElement>) =>{
-        e.preventDefault()
-        setInput(e.target.value)
-    }
-
-    const submitForm = (e: FormEvent<HTMLFormElement>) =>{
-      e.preventDefault()
-      console.log(input)
-    }
-    return (
-        <form onSubmit={submitForm}>
-            <input type="text" 
-            onChange={changeInput}
-            value={input}
-            name="" id="search"/>
-        </form>
-    )
+interface AddCategoryProps {
+  setCategories: Function;
 }
+const AddCategory = ({ setCategories }: AddCategoryProps) => {
+  const [input, setInput] = useState("hey");
+  const changeInput = (e: ChangeEvent<HTMLInputElement>) => {
+    e.preventDefault();
+    setInput(e.target.value);
+  };
 
-export default AddCategory
+  const submitForm = (e: FormEvent<HTMLFormElement>) => {
+    e.preventDefault();
+    if (input.trim().length < 1) {
+      return;
+    }
+    setCategories(oldCateogries => [...oldCateogries, input]);
+    setInput("");
+  };
+  return (
+    <form onSubmit={submitForm}>
+      <input
+        type="text"
+        onChange={changeInput}
+        value={input}
+        name=""
+        id="search"
+      />
+    </form>
+  );
+};
+
+export default AddCategory;
--- GifExpertApp.tsx
+++ GifExpertApp.tsx
@@ -1,20 +1,16 @@
 import React, { Fragment, useState } from "react";
 import Categories from "./Categories";
 import AddCategory from './AddCategory'
+
 const GiftExpertApp = () => {
   const [categories, setCategories] = useState(["Star wars", "Lord of rings"]);
 
-  const handleAdd = () => {
-    const category: string = "HxH"
-    console.log('added')
-    setCategories(oldCategories => [...oldCategories, category]);
-  };
+  
   return (
     <Fragment>
       <h2>Gif Expert App</h2>
       <hr/>
-      <AddCategory></AddCategory>
-      <button onClick={handleAdd}>add</button>
+      <AddCategory setCategories={setCategories}></AddCategory>
       <hr />
       <Categories data={categories} />
     </Fragment>
